49726bbe37efa853f77c2e1b3b4da91a882c6662
Soren Peterson <soren468@gmail.com>
2015-07-24 15:03:01 -0500

    Added tests for reactivity. All tests pasing

--- reactive-var-operations-tests.js
+++ reactive-var-operations-tests.js
@@ -1,9 +1,20 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('ReactiveVar.prototype.add()', function (test) {
+Tinytest.add('Add to the ReactiveVar', function (test) {
 	var someNumber = new ReactiveVar(10);
 	someNumber.add(5);
 	test.equal(someNumber.get(), 15, 'Expected value to be 15');
+});
+
+Tinytest.addAsync('add async', function(test, next) {
+	var someNumber = new ReactiveVar(0);
+	Tracker.autorun(function() {
+		if(someNumber.get() !== 0) {
+			test.notEqual(0, someNumber.get());
+			next();
+		}
+	});
+	someNumber.add(5);
 });
 
 Tinytest.add('ReactiveVar.prototype.subtract()', function (test) {
@@ -12,15 +23,48 @@ Tinytest.add('ReactiveVar.prototype.subtract()', function (test) {
 	test.equal(someNumber.get(), 5, 'Expected value to be 5');
 });
  
+Tinytest.addAsync('subtract async', function(test, next) {
+	var someNumber = new ReactiveVar(0);
+	Tracker.autorun(function() {
+		if(someNumber.get() !== 0) {
+			test.notEqual(0, someNumber.get());
+			next();
+		}
+	});
+	someNumber.subtract(5);
+});
+
 Tinytest.add('ReactiveVar.prototype.multiply()', function (test) {
 	var someNumber = new ReactiveVar(10);
 	someNumber.multiply(5);
 	test.equal(someNumber.get(), 50, 'Expected value to be 50');
 });
 
+Tinytest.addAsync('multiply async', function(test, next) {
+	var someNumber = new ReactiveVar(1);
+	Tracker.autorun(function() {
+		if(someNumber.get() !== 1) {
+			test.notEqual(1, someNumber.get());
+			next();
+		}
+	});
+	someNumber.multiply(5);
+});
+
 Tinytest.add('ReactiveVar.prototype.divide()', function (test) {
 	var someNumber = new ReactiveVar(10);
 	someNumber.divide(5);
 	test.equal(someNumber.get(), 2, 'Expected value to be 2');
+});
+
+Tinytest.addAsync('divide async', function(test, next) {
+	var someNumber = new ReactiveVar(1);
+	Tracker.autorun(function() {
+		if(someNumber.get() !== 1) {
+			test.notEqual(1, someNumber.get());
+			next();
+		}
+	});
+	someNumber.divide(5);
 });
 
--- reactive-var-operations.js
+++ reactive-var-operations.js
@@ -2,17 +2,21 @@
 
 ReactiveVar.prototype.add = function(num) {
 	this.curValue += num;
+	this.dep.changed();
 }
 
 ReactiveVar.prototype.subtract = function(num) {
 	this.curValue -= num;
+	this.dep.changed();
 }
 
 ReactiveVar.prototype.multiply = function(num) {
 	this.curValue *= num;
+	this.dep.changed();
 }
 
 ReactiveVar.prototype.divide = function(num) {
 	this.curValue /= num;
+	this.dep.changed();
 }
 
