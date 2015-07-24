f6971ac40e9f6b613008fdb8f50dd30043d99f31
Soren Peterson <soren468@gmail.com>
2015-07-24 14:34:17 -0500

    all tests pass

--- reactive-var-operations.js
+++ reactive-var-operations.js
@@ -3,3 +3,16 @@
 ReactiveVar.prototype.add = function(num) {
 	this.curValue += num;
 }
+
+ReactiveVar.prototype.subtract = function(num) {
+	this.curValue -= num;
+}
+
+ReactiveVar.prototype.multiply = function(num) {
+	this.curValue *= num;
+}
+
+ReactiveVar.prototype.divide = function(num) {
+	this.curValue /= num;
+}
+
