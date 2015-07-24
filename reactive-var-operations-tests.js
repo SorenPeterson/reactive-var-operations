52cece63db8229f3ac72ab46c9d3374184cc85db
Soren Peterson <soren468@gmail.com>
2015-07-24 15:12:21 -0500

    rewrite tests. failing

--- reactive-var-operations-tests.js
+++ reactive-var-operations-tests.js
@@ -1,70 +1,54 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('Add to the ReactiveVar', function (test) {
-	var someNumber = new ReactiveVar(10);
-	someNumber.add(5);
-	test.equal(someNumber.get(), 15, 'Expected value to be 15');
-});
-
-Tinytest.addAsync('add async', function(test, next) {
-	var someNumber = new ReactiveVar(0);
+Tinytest.addAsync('Add to ReactiveVar', function(test, next) {
+	var initialValue = 1;
+	var someNumber = new ReactiveVar(initialValue);
 	Tracker.autorun(function() {
-		if(someNumber.get() !== 0) {
-			test.notEqual(0, someNumber.get());
+		if(someNumber.get() !== initialValue) {
+			test.notEqual(initialValue, someNumber.get());
 			next();
 		}
 	});
-	someNumber.add(5);
-});
-
-Tinytest.add('ReactiveVar.prototype.subtract()', function (test) {
-	var someNumber = new ReactiveVar(10);
-	someNumber.subtract(5);
-	test.equal(someNumber.get(), 5, 'Expected value to be 5');
+	someNumber.add(6);
+	test.equal(someNumber.get(), 6, 'Expected value to be 6');
 });
  
 Tinytest.addAsync('subtract async', function(test, next) {
-	var someNumber = new ReactiveVar(0);
+	var initialValue = 1;
+	var someNumber = new ReactiveVar(initialValue);
 	Tracker.autorun(function() {
-		if(someNumber.get() !== 0) {
-			test.notEqual(0, someNumber.get());
+		if(someNumber.get() !== initialValue) {
+			test.notEqual(initialValue, someNumber.get());
 			next();
 		}
 	});
 	someNumber.subtract(5);
-});
-
-Tinytest.add('ReactiveVar.prototype.multiply()', function (test) {
-	var someNumber = new ReactiveVar(10);
-	someNumber.multiply(5);
-	test.equal(someNumber.get(), 50, 'Expected value to be 50');
+	test.equal(someNumber.get(), -5, 'Expected value to be -5');
 });
 
 Tinytest.addAsync('multiply async', function(test, next) {
-	var someNumber = new ReactiveVar(1);
+	var initialValue = 1;
+	var someNumber = new ReactiveVar(initialValue);
 	Tracker.autorun(function() {
-		if(someNumber.get() !== 1) {
-			test.notEqual(1, someNumber.get());
+		if(someNumber.get() !== initialValue) {
+			test.notEqual(initialValue, someNumber.get());
 			next();
 		}
 	});
 	someNumber.multiply(5);
-});
-
-Tinytest.add('ReactiveVar.prototype.divide()', function (test) {
-	var someNumber = new ReactiveVar(10);
-	someNumber.divide(5);
-	test.equal(someNumber.get(), 2, 'Expected value to be 2');
+	test.equal(someNumber.get(), 5, 'Expected value to be 5');
 });
 
 Tinytest.addAsync('divide async', function(test, next) {
-	var someNumber = new ReactiveVar(1);
+	var initialValue = 1;
+	var someNumber = new ReactiveVar(initialValue);
 	Tracker.autorun(function() {
-		if(someNumber.get() !== 1) {
-			test.notEqual(1, someNumber.get());
+		if(someNumber.get() !== initialValue) {
+			test.notEqual(initialValue, someNumber.get());
 			next();
 		}
 	});
 	someNumber.divide(5);
+	test.equal(someNumber.get(), 0.2, 'Expected value to be 0.2');
 });
 
