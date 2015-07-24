e5401a9dd397d14dd1787e688a9d45dfda08d88e
Soren Peterson <soren468@gmail.com>
2015-07-24 15:34:08 -0500

    n

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'reactive-var-operations',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'Adds operators to reactive var making them easier to use',
-  // URL to the Git repository containing the source code for this package.
-  git: 'github.com/sorenpeterson/reactive-var-operations',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'sorenpeterson:reactive-var-operations',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: 'Adds operators to reactive var making them easier to use',
+		// URL to the Git repository containing the source code for this package.
+	git: 'github.com/sorenpeterson/reactive-var-operations',
+		// By default, Meteor will default to using README.md for documentation.
+		// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.use('reactive-var');
-  api.addFiles('reactive-var-operations.js');
+	api.versionsFrom('1.1.0.2');
+	api.use('reactive-var');
+	api.addFiles('reactive-var-operations.js');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('reactive-var');
-  api.use('reactive-var-operations');
-  api.addFiles('reactive-var-operations-tests.js');
+	api.use('tinytest');
+	api.use('reactive-var');
+	api.use('reactive-var-operations');
+	api.addFiles('reactive-var-operations-tests.js');
 });
